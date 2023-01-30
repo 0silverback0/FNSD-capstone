afea92a88d57c7113badb7428daa6435607f1cd8
0silverback0 <fourmarz@yahoo.com>
2023-01-30 06:35:29 -0500

    render deploy error

--- auth.py
+++ auth.py
@@ -3,10 +3,10 @@ from flask import request, _request_ctx_stack, jsonify
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
-from config import AUT0_DOMAIN
+#from config import AUT0_DOMAIN
 import os
 
-AUTH0_DOMAIN = os.getenv(AUTH0_DOMAIN, 'dev-rk1g1wysbyoswvka.us.auth0.com')
+AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN', 'dev-rk1g1wysbyoswvka.us.auth0.com')
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'capstone'
 
